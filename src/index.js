880ee0f986507aaeb402cf3c9fa4c02c0ca3ee0c
pewcia <ewelina.serafin996@gmail.com>
2025-04-10 16:57:27 +0200

    added java script

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "bb203f4a92e7t89f33c44201bd2fbfoa";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
